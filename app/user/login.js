1c29ac4bfd42234f509a38e954761e7684cf08b2
Peter <monroepe@gmail.com>
2016-08-28 16:44:35 -0500

    add login

--- app/user/login.js
+++ app/user/login.js
@@ -11,7 +11,17 @@ angular.module('expenseTrak.logIn', ['ngRoute'])
 
 .controller('LogInCtrl', ['$scope', '$location', function($scope, $location) {
 
-  $scope.logInFormSubmit = function(){
+  $scope.logInFormSubmit = function() {
+    firebase.auth().signInWithEmailAndPassword($scope.email, $scope.password)
+      .then(function() {
+        $scope.$apply(function() { $location.path("/addExpense"); });
+      })
+      .catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log('Error: ' + errorCode + ': ' + errorMessage);
+        clearForm();
+      });
   }
 
   function loginFailed(){
